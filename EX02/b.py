86ea23e49a7777aa23135526840d1ad613e4b4b2
Gabriel <gabrielvazcancadoferreira@gmail.com>
2025-05-12 16:30:32 +0200

    Solving letter C with RK45 is taking too much time

--- EX02/b.py
+++ EX02/b.py
@@ -1,30 +1,34 @@
 import numpy as np
 import math
 
-from a import p
+from a import p as parameters
 
-def f(t,guess,p):
+def f(t,guess):
+
+    p = parameters()
 
     Ca = guess[0]
     Cb = guess[1]
     Cc = guess[2]
     Cd = guess[3]
 
-    dCa = (p.A0/p.q)*(-p.k10*(math.e**(-p.Ea1/(p.R*p.T)))*(Ca**(p.n1)))
+    rate1 = p.k10 * np.exp(-p.Ea1 / (p.R * p.T))
+    rate2 = p.k20 * np.exp(-p.Ea2 / (p.R * p.T))
+    rate3 = p.k30 * np.exp(-p.Ea3 / (p.R * p.T))
+    factor = p.A0 / p.q
 
-    dCb = (p.A0/p.q)*(p.k10*(math.e**(-p.Ea1/(p.R*p.T)))*(Ca**(p.n1)) -p.k20*(math.e**(-p.Ea2/(p.R*p.T)))*(Cb**p.n2) -p.k30*(math.e**(-p.Ea3/(p.R*p.T)))*(Cb**p.n3))
-    
-    dCc = (p.A0/p.q)*(p.k20*(math.e**(-p.Ea2/(p.R*p.T)))*(Cb**(p.n2)))
+    dCa = factor * (-rate1 * Ca**p.n1)
+    dCb = factor * (rate1 * Ca**p.n1 - rate2 * Cb**p.n2 - rate3 * Cb**p.n3)
+    dCc = factor * (rate2 * Cb**p.n2)
+    dCd = factor * (rate3 * Cb**p.n3)
 
-    dCd = (p.A0/p.q)*(p.k30*(math.e**(-p.Ea3/(p.R*p.T)))*(Cb**(p.n3)))
-
-    gradients = np.array([dCa,dCb,dCc,dCd])
+    gradients = [dCa,dCb,dCc,dCd]
 
     return gradients
 
 
 if __name__ == '__main__':
 
-    P = p()
-    print('oi')
+    P = parameters()
     gradients = f(P,[P.CaIn,P.CbIn,P.CcIn,P.CdIn])
+    print(gradients)
--- EX02/c.py
+++ EX02/c.py
@@ -3,41 +3,34 @@ import math
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-from a import p
+from a import p as parameters
 from b import f
 
 import time
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    P = p()
-    y0 = [P.CaIn, P.CbIn, P.CcIn, P.CdIn]
-    t0, tf = 0, 50
-    t_eval = np.linspace(t0, tf, 100)
 
-    print("Solving ODE system")
+    p = parameters()
 
-    start = time.time()
+    y0 = [p.CaIn,p.CbIn,p.CcIn,p.CdIn]
 
-    sol = solve_ivp(lambda t, y: f(t, y, P), (t0, tf), y0, method='RK45', dense_output=False)
+    x_span = (0, 50)
+    x_eval = np.linspace(*x_span, 50)
 
-    concentrations = []
-    for t in tqdm(t_eval):
-        concentrations.append(sol.sol(t))
+    sol = solve_ivp(f, x_span, y0, method='RK45', t_eval=x_eval, max_step = 0.001, rtol = 1e-6,atol = 1e-8) 
 
-    end = time.time()
-    print(f"Solução completada em {end - start:.2f} segundos.")
+    t = sol.t
+    y1, y2, y3, y4 = sol.y
 
-    concentrations = np.array(concentrations).T
-
-    print(concentrations)
-    plt.plot(t_eval, concentrations[0], label='Ca')
-    plt.plot(t_eval, concentrations[1], label='Cb')
-    plt.plot(t_eval, concentrations[2], label='Cc')
-    plt.plot(t_eval, concentrations[3], label='Cd')
-    plt.xlabel('Position [m]')
-    plt.ylabel('Gradient [mol/m³]')
-    plt.title('Gradient vs Position')
+    plt.figure(figsize=(10, 6))
+    plt.plot(t, y1, label='y1')
+    plt.plot(t, y2, label='y2')
+    plt.plot(t, y3, label='y3')
+    plt.plot(t, y4, label='y4')
+    plt.xlabel('Time t')
+    plt.ylabel('Solutions')
+    plt.title('Solution of 4 ODEs using RK45')
     plt.legend()
-    plt.grid()
+    plt.grid(True)
     plt.show()
